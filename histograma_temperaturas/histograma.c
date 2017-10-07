2f566c78c7ee43629a15dfce0b35f1d8601bdc93
Carlos Maniusis Jr <junior.maniusis@gmail.com>
2017-10-07 10:16:12 -0300

    comentarios adicionados

--- histograma_temperaturas/histograma.c
+++ histograma_temperaturas/histograma.c
@@ -1,37 +1,31 @@
 #include <stdio.h>
 int main(){
-	double t[20000];
-	int i=0;
-	printf("Iniciando a leitura..\n");
-	while( scanf("%lf", &t[i]) > 0){
-		//lê os dados
+	double t[20000];//array de temperaturas
+	
 
-		//printf("t[%d]=%lf\n",i,t[i] );
+	//leitura dos dados, a entrada padrao será redirecionada
+	//para o arquivo temperatura.csv
+	int i=0; //conta o numero de temperatudas realmente lidos
+	while( scanf("%lf", &t[i]) > 0){
 		i++;
 	}
 
-	//processamento
-	
+
+	//calcula o maximo, minimo e a média
 	double  med=0, min=t[0], max=t[0],soma=0;
 	for(int j =0;j<i;j++){
-		
 		soma += t[j];
-
 		if (t[j] > max)
 			max = t[j];
 		if (t[j] < min){
 			min = t[j];
-			//printf("MINIMO=%lf\n",min);
 		}
-
-
-
-
 	}
-	//for(int j =0;j<i;j++){printf("%lf  -  %d\n",t[j],j );}
 	med = soma/i;
 	printf("med = %.3lf, min = %.3lf, max = %.3lf\n", med,min,max);
-	//printf("Foram realizadas %d leituras\n",i );
+	
+
+
 	/*media das temperaturas acima da media*/
 	soma=0;
 	int n_temp_acima_med=0;
@@ -43,6 +37,9 @@ int main(){
 	}
 	double med_acima_media = soma/n_temp_acima_med;
 	printf("Média das temperaturas acima da média: %.3lf\n",  med_acima_media);
+
+	
+
 
 	
 	return 0;
